f27fed7175d8a76746f3b4c9263245b4e5b84f79
StevenKowalzik <stvnkwlzk@googlemail.com>
2019-11-30 16:54:47 +0100

    dockerfile & captain-definition file

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ import bodyParser from 'body-parser';
 import cors from 'cors';
 import foods from './routes/foods';
 
-const { PORT = 2020 } = process.env;
+const { PORT = 3000 } = process.env;
 
 express()
   .use(bodyParser.json())
