9dd07fc39554076327cdd5b5cc5e2978d82093e8
Steven Kowalzik <stvnkwlzk@googlemail.com>
2021-02-09 14:00:27 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import app from './server'
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => {
-  console.log(`> Server is listening on port ${PORT}`);
-});
+  console.log(`> Server is listening on port ${PORT}`)
+})
